ae8c1846af3a0388d0009ae5542014eba202b02c
xpadev <xpadev@gmail.com>
2025-11-12 23:35:35 +0900

    fix: remove ts error

--- electron/store.ts
+++ electron/store.ts
@@ -1,6 +1,4 @@
 import * as ElectronStore from "electron-store";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-expect-error
 const store = new ElectronStore() as ElectronStore<Record<string, unknown>>;
 export { store };
